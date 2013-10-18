415679f83fb5d03a1b09ff28b0d4cb8e648da439
Toomas Korka <evyumble@gmail.com>
2013-10-18 08:12:32 +0300

    'commenthere'

--- src/ee/ut/math/tvt/bartersmart/Intro.java
+++ src/ee/ut/math/tvt/bartersmart/Intro.java
@@ -3,20 +3,43 @@ package ee.ut.math.tvt.bartersmart;
 import java.io.IOException;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
+import ee.ut.math.tvt.bartersmart.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.bartersmart.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.bartersmart.ui.ConsoleUI;
+import ee.ut.math.tvt.bartersmart.ui.SalesSystemUI;
 
 public class Intro {
 
-	/**
-	 * @param args
-	 */
-	static IntroUI myPOS;
 	private static final Logger log = Logger.getLogger(Intro.class);
+	private static final String MODE = "console";
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		DOMConfigurator.configure("etc/log4j.xml");
-		myPOS = new IntroUI();
-		log.info("Intro started");
-	}
 
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+		DOMConfigurator.configure("etc/log4j.xml");
+
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			IntroUI introUI = new IntroUI();
+			introUI.setVisible(true);
+			introUI.setAlwaysOnTop(true);
+
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+			log.info("Intro started");
+
+			introUI.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			introUI.setVisible(false);
+		}
+	}
 }
